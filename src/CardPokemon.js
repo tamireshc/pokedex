8bda6d3f9180cb88b52f1b6ed5efd75a3950d963
tamireshc <tamireshc@gmail.com>
2022-05-07 00:49:10 -0300

    fix layout

--- src/CardPokemon.js
+++ src/CardPokemon.js
@@ -3,22 +3,22 @@ import React from 'react';
 class CardPokemon extends React.Component {
   render() {
     const { data } = this.props;
-    console.log(data);
+    // console.log(data);
     const { name, type, averageWeight, image } = data;
 
     return (
       <div className='pokecard'>
+        <div className='pokeIMG'>
+          <img src={image} alt='' srcset='' />
+        </div>
         <div className='infos'>
           <p className='name'>{name}</p>
-          <p>{type}</p>
+          <p className='type'>{type}</p>
           <p className='weight'>
-            AverageWeight:
+            Weight:
             <span>{averageWeight.value}</span>
             <span>{averageWeight.measurementUnit}</span>
           </p>
-        </div>
-        <div className='pokeIMG'>
-          <img src={image} alt='' srcset='' />
         </div>
       </div>
     );
